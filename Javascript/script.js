8bf1aad5efbcad742cdd3b40435b5dbc8cdb93d8
lipika <lipikaramasamy@gmail.com>
2026-06-04 15:39:52 +0530

    javascript completed

---
+++ Javascript/script.js
@@ -0,0 +1,101 @@
+// var a= 10;
+// let b= 20;
+// const c= 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num = 10;
+// var str = "lipika";
+// var undefine;
+// var Null = null;
+// var bollean = true;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof undefine);
+// console.log(typeof  Null);
+
+
+// var bidInt = 123n;
+// var symbol = Symbol('li');
+
+
+// console.log(typeof bigInt);
+// console.log(typeof symbol);
+
+
+// var arr = [10,20,30,40];
+
+// console.log(typeof arr,arr);
+// var obj = {
+//     name:"lipika",
+//     dept:"AIDS"
+// }
+// console.log(typeof obj,obj)
+
+//arithemetic operator(+,-,*,/,%)
+
+// var a = 10;
+// var b = "laks";
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(a/b);
+
+//logical operator (&&,||!)
+//                &&     ||
+//true true      true   true
+//true false     false  true
+//false true     false  true
+//false false    false  false
+
+// var a = true;
+// var b = false;
+
+// console.log(a&&b);
+
+// //relational operator(<,>,<=,>=,)
+
+// a = 10;
+// b = 20;
+
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+
+// //asignment operator(=,+=,-=,*=,%=,/=,!=,**=)
+// a = 10;
+// b = 20;
+// a = a+b;
+// a+=b;
+// console.log(a);
+// a-=b;
+// console.log(a);
+
+//unary operator(--,++)
+
+// var a = 10;
+
+// console.log(a++);
+// console.log(++a);
+
+// var a=10;
+
+// console.log(a--);
+// console.log(--a);
+
+// a=1;
+//  b=1;
+// c=0;
+
+// var result = a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+// console.log(result);
+
